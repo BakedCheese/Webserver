c7d1d866bcb6fa5cc8b540246990a91a172e99d2
BakedCheese <48770243+BakedCheese@users.noreply.github.com>
2022-01-19 22:00:51 +0100

    Update CustomSql.js

--- Admin/CustomSql.js
+++ Admin/CustomSql.js
@@ -1,8 +1,8 @@
 import dbConnection from "../connection.js";
 
 export const CustomQuery = (req, res) => {
-  console.log([req.body.query]);
-  dbConnection.query("?", [req.body.query], (err, results) => {
+  console.log(req.body.query);
+  dbConnection.query(`${req.body.query}`, (err, results) => {
     if (!err) {
       res.json(results);
     } else {
