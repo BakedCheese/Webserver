4e4a3bc77f269d61b84eb1e4d2c416e3fd3e1c9a
BakedCheese <48770243+BakedCheese@users.noreply.github.com>
2022-03-03 12:01:59 +0100

    Testing a fix

--- connection.js
+++ connection.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2";
 import fs from "fs";
+import cron from "node-cron";
 
 
-
-const con = mysql.createConnection({
+let con = mysql.createConnection({
   host: "portfolio-do-user-8793639-0.b.db.ondigitalocean.com",
   user: "doadmin",
   password: "7t1wsx3tNIPLPHIy",
@@ -14,14 +14,36 @@ const con = mysql.createConnection({
   }
 });
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-  
+Checkconnection();
+
+
+
+cron.schedule('* * */2 * *', () => {
+  console.log('Reconnecting module actived -->');
+  con.end();
+  console.log('Connection ended -->');
+  console.log('Connecting...');
+  con = mysql.createConnection({
+    host: "portfolio-do-user-8793639-0.b.db.ondigitalocean.com",
+    user: "doadmin",
+    password: "7t1wsx3tNIPLPHIy",
+    port: "25060",
+    database: "defaultdb",
+    ssl  : {
+      ca : fs.readFileSync('ca-certificate.crt')
+    }
+  });
+  Checkconnection();
 });
 
-con.end();
+function Checkconnection(){
 
+  const now = new Date();
 
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected! on: "  + now);
+  });
+}
 
 export default con;
